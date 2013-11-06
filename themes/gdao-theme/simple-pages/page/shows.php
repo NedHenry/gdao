4f9fbb01b4213f3f4538e2c54f18cf794f8b9df5
Kevin S. Clarke <ksclarke@gmail.com>
2013-11-05 23:51:54 -0500

    testing swapping in api

--- themes/gdao-theme/simple-pages/page/shows.php
+++ themes/gdao-theme/simple-pages/page/shows.php
@@ -1,9 +1,16 @@
-<?php $host = $_SERVER['SERVER_NAME']; // check which machine we're on... ?>
+<script type='text/javascript' src="https://maps.googleapis.com/maps/api/js?sensor=false"></script>
+<script type='text/javascript'>
+        $(function(){
+            var myOptions = {
+                center: new google.maps.LatLng(42.811522,47.285156),
+                zoom: 10,
+                mapTypeId: google.maps.MapTypeId.TERRAIN
+            };
+            gMap = new google.maps.Map(document.getElementById('gdao_map_canvas'), myOptions);
 
-<?php if (strpos($host, 'library.ucsc') === false): ?>
-  <iframe width="950" height="400" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"
-  src="maps.google.com/maps/ms?msa=0&amp;msid=206723407710575786600.0004c32be5bc05f7a0b81&amp;ie=UTF8&amp;t=h&amp;ll=42.55308,-48.339844&amp;spn=55.121316,166.816406&amp;z=3&amp;output=embed"></iframe>
-<?php else: ?>
-  <iframe width="950" height="400" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"
-  src="maps.google.com/maps/ms?msa=0&amp;msid=206723407710575786600.0004c32c00381f43c5bba&amp;ie=UTF8&amp;t=h&amp;ll=42.811522,-47.285156&amp;spn=54.926026,166.816406&amp;z=3&amp;output=embed"></iframe>
-<?php endif; ?>
+            var kmlLayer = new google.maps.KmlLayer('https://maps.google.com/maps/ms?ie=UTF8&t=m&authuser=0&msa=0&output=kml&msid=206723407710575786600.0004c32c00381f43c5bba');
+            kmlLayer.setMap(gMap);
+        });
+</script>
+
+<div id="gdao_map_canvas" style="width: 400px; height: 400px;"></div>
