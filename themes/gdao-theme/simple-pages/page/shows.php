ba22d4039054d0b37bf07e191167e90d830ba7de
Kevin S. Clarke <ksclarke@gmail.com>
2013-11-06 00:09:29 -0500

    fixed which map is referenced

--- themes/gdao-theme/simple-pages/page/shows.php
+++ themes/gdao-theme/simple-pages/page/shows.php
@@ -1,4 +1,4 @@
 <iframe width="950" height="400" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"
-  src="https://maps.google.com/maps/ms?msa=0&amp;msid=206723407710575786600.0004c32c00381f43c5bba&amp;ie=UTF8&amp;ll=42.811522,-47.285156&amp;t=h&amp;spn=54.926026,166.816406&amp;z=3&amp;output=embed"></iframe>
+  src="https://maps.google.com/maps/ms?msa=0&amp;msid=206723407710575786600.0004c32be5bc05f7a0b81&amp;ie=UTF8&amp;t=h&amp;ll=42.55308,-48.339844&amp;spn=55.121316,166.816406&amp;z=3&amp;output=embed"></iframe>
 
 <div id="gdao_map_canvas" style="width: 400px; height: 400px;"></div>
